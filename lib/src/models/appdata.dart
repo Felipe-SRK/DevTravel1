d45187f701582b08d79a263cc9b11937a1821eae
Felipe Silva <felipesilva257@gmail.com>
2021-05-07 12:53:16 -0300

    B7 Web

---
+++ lib/src/models/appdata.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'dart:core';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class AppData with ChangeNotifier {
+  var data = [];
+
+  void setData(newData) {
+    data = newData;
+    notifyListeners();
+  }
+
+  Future<bool> requestData() async {
+    final res =
+        await http.get(Uri.http('https://api.b7web.com.br', '/flutter1wb/'));
+
+    if (res.statusCode == 200) {
+      setData(jsonDecode(res.body));
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
